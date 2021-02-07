74f5a734c3e2eb5c1aad36103f7662706bc57007
Leah In <23352423+leahin@users.noreply.github.com>
2021-02-07 09:28:33 -0500

    Moved scripts to js files

--- public/scripts/add_order.js
+++ public/scripts/add_order.js
@@ -1,1 +1,31 @@
 // js for add_order.html
+
+function addProduct(){
+  var form = document.getElementById("productOrderAdd");
+
+  var div0 = document.createElement("div");
+  div0.setAttribute("class", "form-row");
+  form.appendChild(div0);
+
+  var div1 = document.createElement("div");
+  div1.setAttribute("class", "col-md-1 md-1");
+  var label1 = document.createElement("label")
+  label1.innerHTML = "Product Id";
+  var input1 = document.createElement("input")
+  input1.setAttribute("type", "text");
+  input1.setAttribute("class", "form-control");
+  div1.appendChild(label1);
+  div1.appendChild(input1);
+  div0.appendChild(div1);
+
+  var div2 = document.createElement("div");
+  div2.setAttribute("class", "col-md-1 md-1");
+  var label2 = document.createElement("label")
+  label2.innerHTML = "QTY";
+  var input2 = document.createElement("input")
+  input2.setAttribute("type", "text");
+  input2.setAttribute("class", "form-control");
+  div2.appendChild(label2);
+  div2.appendChild(input2);
+  div0.appendChild(div2);
+}
--- public/scripts/o_scripts.js
+++ public/scripts/o_scripts.js
@@ -1,1 +1,7 @@
 // js for orders.html
+
+var myWindow;
+
+function openWin() {
+  myWindow = window.open("orders_products.html", "_blank", "width=1000, height=800");
+}

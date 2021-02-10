2416a12250e10c0808bb77a3a98d49ec2882629d
terrenceosu <59637780+terrenceosu@users.noreply.github.com>
2021-02-10 00:56:53 -0800

    updated with sql data

--- Step4_sql_assignment/Step4_ABC_DB.sql
+++ Step4_sql_assignment/Step4_ABC_DB.sql
@@ -1,5 +1,3 @@
-
-
 -- Create Stores Table
 CREATE TABLE abc_stores (
     store_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -7,9 +5,6 @@ CREATE TABLE abc_stores (
     street_address VARCHAR(255) NOT NULL,
     state VARCHAR(3) NOT NULL,
     zip_code VARCHAR(5) NOT NULL);
-
-
-DESCRIBE abc_stores
 
 -- Create Products Table
 CREATE TABLE abc_products (
@@ -39,13 +34,32 @@ CREATE TABLE abc_orders (
     );
 
 
---Create Orders_products Table
+-- Create Orders_products Table
 CREATE TABLE abc_orders_products (
     pid INT,
     oid INT NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY(pid) REFERENCES abc_products(product_id),
     FOREIGN KEY(oid) REFERENCES abc_orders(order_id)
-    
+
     );
 
+-- Dumping Data for stores
+
+INSERT into abc_stores (store_name, street_address, state, zip_code) VALUES
+('ABC Downtown Seattle', '500 Pine Street', 'WA', '98101'),
+('ABC New York', '225 W 57th St', 'NY', '10019'),
+('ABC The Grove Los Angeles', '189 The Grove Drive', 'CA', '90036'),
+('ABC Downtown Portland', '701 SW Broadway', 'OR', '97205'),
+('ABC Houston Galleria', '5192 Hidalgo Street', 'TX', '77056'),
+('ABC Galleria Dallas', '5220 Alpha Road', 'TX', '75240'),
+('ABC Houston Galleria', '5192 Hidalgo Street', 'TX', '77056'),
+('ABC Boston', '497 Boylston St', 'MA', '02116')
+;
+
+-- Dumping Data for Customers
+
+INSERT into abc_customers (first_name, last_name, birthday) VALUES
+('Terrence', 'Dominguez-Smith', 12/23/1985)
+
+;

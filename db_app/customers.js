7f9bf638e37c85941bcaae7ac2d5df65373b9ac2
Leah In <23352423+leahin@users.noreply.github.com>
2021-02-20 16:25:45 -0500

    Modified customers page
    changed format

--- db_app/customers.js
+++ db_app/customers.js
@@ -1,10 +1,9 @@
+module.exports = function(){
 
-var express = require('express');
-var router = express.Router();
+  var express = require('express');
+  var router = express.Router();
 
-module.exports = function(router){
-
-  router.get('/customers', function(req, res){
+  router.get('/', function(req, res){
     var context = {'title': 'Customers', 'script': 'customers'};
 
 
@@ -13,29 +12,30 @@ module.exports = function(router){
 
     // TODO: delete dummyData
     var dummyData = [
-      {'id': '1', 'fname': 'Emma', 'lname': 'Woodhouse', 'birthdate': '2001-01-01'},
-      {'id': '2', 'fname': 'Jack', 'lname': 'Tyler', 'birthdate': '2002-02-02'},
-      {'id': '3', 'fname': 'Jane', 'lname': 'Hudson', 'birthdate': '2003-03-03'},
+      {id: '1', fname: 'Emma', lname: 'Woodhouse', birthdate: '2001-01-01'},
+      {id: '2', fname: 'Jack', lname: 'Tyler', birthdate: '2002-02-02'},
+      {id: '3', fname: 'Jane', lname: 'Hudson', birthdate: '2003-03-03'},
     ];
 
-    context['customerList'] = dummyData;
+    context['inputList'] = dummyData;
     JSON.stringify(context);
     res.render('customers', context);
   });
 
 
-  router.post('/customers', function(req, res){
-    // TODO: insert INTO
-  });
+  // router.post('/customers', function(req, res){
+  //   // TODO: insert INTO
+  // });
+  //
+  //
+  // router.put('/customers', function(req, res){
+  //   // TODO: update
+  // });
+  //
+  //
+  // router.delete('/customers', function(req, res){
+  //   // TODO: delete
+  // });
 
-
-  router.put('/customers', function(req, res){
-    // TODO: update
-  });
-
-
-  router.delete('/customers', function(req, res){
-    // TODO: delete
-  });
-
-};
+  return router;
+}();
--- public/scripts/customers.js
+++ public/scripts/customers.js
@@ -1,2 +1,1 @@
-// js fo rcustomers.html
 navCurrent("navCustomers");

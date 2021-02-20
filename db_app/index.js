d56f9bf6c03ed19374c0751b4cff25a14336ffe8
Leah In <23352423+leahin@users.noreply.github.com>
2021-02-20 16:29:07 -0500

    Update index.js
    index.js completed. do not alter.

--- db_app/index.js
+++ db_app/index.js
@@ -1,52 +1,37 @@
-
 var express = require('express');
-var app = express();
-var router = express.Router();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var path = require('path');
+var mysql = require('./dbcon.js');
 
+var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.use(express.urlencoded({extended: false}));
+app.set('mysql', mysql);
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.use('/', router);
-require('./stores')(router);
-require('./products')(router);
-require('./customers')(router);
-require('./orders')(router);
-require('./sales')(router);
-require('./stores_products')(router);
-require('./customers_products')(router);
+// app port
+app.set('port', 7892);
+app.listen(app.get('port'), function() {
+  console.log(
+    `Express started on http://${process.env.HOSTNAME}:${app.get('port')};
+     press Ctrl-C to terminate.`);
+});
+
+app.use('/stores', require('./stores'));
+app.use('/products', require('./products'));
+app.use('/orders', require('./orders'));
+app.use('/customers', require('./customers'));
+app.use('/sales', require('./sales'));
+app.use('/stores_products', require('./stores_products'));
+app.use('/customers_products', require('./customers_products'));
 app.use(express.static('../public'));
 
-app.set('port', 7892);
-
-
-// routers
-router.get('/', function(req, res){
-  var context = {'title': 'Home', 'script': 'home'};
-  JSON.stringify(context);
+app.get('/', function(req, res){
+  var context = {'title': 'Home', 'jsscripts': ['scripts', 'home']};
   res.render('home', context);
 });
 
-// app.get('/add_orders', function(req, res){
-//   res.render('add_orders.html');
-// });
-//
-// app.get('/orders_products', function(req, res){
-//   res.sendFile(path.join(__dirname + '/orders_products.html'));
-// });
-
-
-// post
-app.post('/', function(req, res){
-  res.render('home');
-});
-
-
-
-// error handlers
 app.use(function(req, res) {
   res.status(404);
   res.render('404');
@@ -56,12 +41,4 @@ app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render('500');
-});
-
-
-// listen
-app.listen(app.get('port'), function() {
-  console.log(
-    `Express started on http://${process.env.HOSTNAME}:${app.get('port')};
-     press Ctrl-C to terminate.`);
 });

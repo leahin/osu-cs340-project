03afd9e00da46925000e12ecbbe846e6e3e39a71
Leah In <23352423+leahin@users.noreply.github.com>
2021-02-20 16:22:53 -0500

    Modified stores page
    changed format a little bit.

--- db_app/stores.js
+++ db_app/stores.js
@@ -1,10 +1,8 @@
+module.exports = function(){
+  var express = require('express');
+  var router = express.Router();
 
-var express = require('express');
-var router = express.Router();
-
-module.exports = function(router){
-
-  router.get('/stores', function(req, res){
+  router.get('/', function(req, res){
     var context = {'title': 'Stores', 'script': 'stores'};
 
 
@@ -13,29 +11,30 @@ module.exports = function(router){
 
     // TODO: delete dummyData
     var dummyData = [
-      {'id': '1', 'name': 'Store 1', 'address': '1111 Store St.', 'state': 'OR', 'zipcode': '99999'},
-      {'id': '2', 'name': 'Store 2', 'address': '2222 Store St.', 'state': 'OR', 'zipcode': '99999'},
-      {'id': '3', 'name': 'Store 3', 'address': '3333 Store St.', 'state': 'OR', 'zipcode': '99999'},
+      {id: '1', name: 'Store 1', address: '1111 Store St.', state: 'OR', zipcode: '99999'},
+      {id: '2', name: 'Store 2', address: '2222 Store St.', state: 'OR', zipcode: '99999'},
+      {id: '3', name: 'Store 3', address: '3333 Store St.', state: 'OR', zipcode: '99999'},
     ];
 
-    context['storeList'] = dummyData;
+    context['inputList'] = dummyData;
     JSON.stringify(context);
     res.render('stores', context);
   });
 
 
-  router.post('/stores', function(req, res){
-    // TODO: insert INTO
-  });
+  // router.post('/stores', function(req, res){
+  //   // TODO: insert INTO
+  // });
+  //
+  //
+  // router.put('/stores', function(req, res){
+  //   // TODO: update
+  // });
+  //
+  //
+  // router.delete('/stores', function(req, res){
+  //   // TODO: delete
+  // });
 
-
-  router.put('/stores', function(req, res){
-    // TODO: update
-  });
-
-
-  router.delete('/stores', function(req, res){
-    // TODO: delete
-  });
-
-};
+  return router;
+}();

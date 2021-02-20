5bc3710a4af6602634af043e9cdfafbbd89fe751
Leah In <23352423+leahin@users.noreply.github.com>
2021-02-20 16:28:02 -0500

    Modified cp and sp
    changed the format

--- db_app/customers_products.js
+++ db_app/customers_products.js
@@ -1,13 +1,13 @@
-
-var express = require('express');
-var router = express.Router();
-
 module.exports = function(router){
 
-  router.get('/customers_prodcuts', function(req, res){
+  var express = require('express');
+  var router = express.Router();
+
+  router.get('/', function(req, res){
     var context = {'title': 'Customers and Products', 'script': 'customers_products'};
     JSON.stringify(context);
     res.render('customers_products', context)
   });
 
-};
+  return router;
+}();
--- db_app/stores_products.js
+++ db_app/stores_products.js
@@ -1,13 +1,13 @@
-
-var express = require('express');
-var router = express.Router();
-
 module.exports = function(router){
 
-  router.get('/stores_products', function(req, res){
+  var express = require('express');
+  var router = express.Router();
+
+  router.get('/', function(req, res){
     var context = {'title': 'Stores and Prodcuts', 'script': 'stores_products'};
     JSON.stringify(context);
     res.render('stores_products', context)
   });
 
-};
+  return router;
+}();

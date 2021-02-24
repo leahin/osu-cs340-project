b104427495eb2c22bf62132d1503e1061b7c7722
terrenceosu <59637780+terrenceosu@users.noreply.github.com>
2021-02-24 00:33:24 -0800

    Customers Reading from db correctly.

--- db_app/customers.js
+++ db_app/customers.js
@@ -3,24 +3,61 @@ module.exports = function(){
   var express = require('express');
   var router = express.Router();
 
-  router.get('/', function(req, res){
-    var context = {'title': 'Customers', 'script': 'customers'};
+  function getCustomers(res, mysql, context, complete) {
+    mysql.pool.query("SELECT customer_id, first_name, last_name, birthdate FROM abc_customers", function (error, results, fields) {
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.customers = results;
+      complete();
+    });
+  }
+
+
+  router.get('/', function (req, res) {
+    var callbackCount = 0;
+    var context = {};
+    var mysql = req.app.get('mysql');
+
+    getCustomers(res, mysql, context, complete);
+
+    function complete() {
+      callbackCount++;
+      if (callbackCount >= 1) {
+        res.render('customers', context);
+      }
+
+    }
+  });
+
+
+
+
+  return router;
+}();
+
+
+
+
+  // router.get('/', function(req, res){
+  //   var context = {'title': 'Customers', 'script': 'customers'};
 
 
     // TODO: sql statement goes here...
     // TODO: use loop to create an array & mapping
 
     // TODO: delete dummyData
-    var dummyData = [
-      {id: '1', fname: 'Emma', lname: 'Woodhouse', birthdate: '2001-01-01'},
-      {id: '2', fname: 'Jack', lname: 'Tyler', birthdate: '2002-02-02'},
-      {id: '3', fname: 'Jane', lname: 'Hudson', birthdate: '2003-03-03'},
-    ];
+  //   var dummyData = [
+  //     {id: '1', fname: 'Emma', lname: 'Woodhouse', birthdate: '2001-01-01'},
+  //     {id: '2', fname: 'Jack', lname: 'Tyler', birthdate: '2002-02-02'},
+  //     {id: '3', fname: 'Jane', lname: 'Hudson', birthdate: '2003-03-03'},
+  //   ];
 
-    context['inputList'] = dummyData;
-    JSON.stringify(context);
-    res.render('customers', context);
-  });
+  //   context['inputList'] = dummyData;
+  //   JSON.stringify(context);
+  //   res.render('customers', context);
+  // });
 
 
   // router.post('/customers', function(req, res){
@@ -37,5 +74,3 @@ module.exports = function(){
   //   // TODO: delete
   // });
 
-  return router;
-}();
--- db_app/stores.js
+++ db_app/stores.js
@@ -6,27 +6,22 @@ module.exports = function(){
    // var context = {'title': 'Stores', 'script': 'stores'};
 
 
-    // TODO: sql statement goes here...
-    // TODO: use loop to create an array & mapping
 
-    // TODO: delete dummyData
-
-    function getStores(res, mysql, context, complete){
-      mysql.pool.query("SELECT store_id, store_name, street_address, state, zip_code FROM abc_stores", function(error, results, fields){
-          if(error){
-              res.write(JSON.stringify(error));
-              res.end();
-          }
-          context.stores = results;
-          complete();
-      });
+  function getStores(res, mysql, context, complete) {
+    mysql.pool.query("SELECT store_id, store_name, street_address, state, zip_code FROM abc_stores", function (error, results, fields) {
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.stores = results;
+      complete();
+    });
   }
 
 
   router.get('/', function (req, res) {
     var callbackCount = 0;
     var context = {};
-    // context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
     var mysql = req.app.get('mysql');
 
     getStores(res, mysql, context, complete);

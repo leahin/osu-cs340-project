687b287d8072918ee5a0077f460f3d348dff4f26
Leah In <23352423+leahin@users.noreply.github.com>
2021-02-24 10:57:47 -0500

    Delete dbcon.js

--- db_app/dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_domingte',
-  password        : '6580',
-  database        : 'cs340_domingte'
-});
-module.exports.pool = pool;

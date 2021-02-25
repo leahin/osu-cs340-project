e024d726d69199cc47919dcfe6ca9a1d99f83c81
terrenceosu <59637780+terrenceosu@users.noreply.github.com>
2021-02-25 02:14:53 -0800

    Changed the script file name for stores.  No imactful changes.  Couldnt get search to work.

---
+++ public/scripts/scriptStores.js
@@ -0,0 +1,4 @@
+function searchStore(){
+    var name = document.getElementById('store_name').value + document.getElementById('state').value;
+    window.location = '/stores/search/' + encodeURI(name);
+}

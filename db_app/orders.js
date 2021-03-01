d787c695f1b0c811524f115893247434a01cc7d2
Leah In <23352423+leahin@users.noreply.github.com>
2021-03-01 15:55:29 -0500

    finished orders
    added comment and update input validation

--- db_app/orders.js
+++ db_app/orders.js
@@ -2,18 +2,27 @@ module.exports = function(){
   var express = require('express');
   var router = express.Router();
 
+  // READ/UPDATE/DELETE queries for Orders
   var getQuery = 'SELECT * FROM abc_orders ORDER BY order_id DESC';
   var searchQuery = 'SELECT * FROM abc_orders WHERE ';
+  var updateQuery = 'UPDATE abc_orders SET cid = ?, sid = ?, order_date = ? WHERE order_id = ?';
   var deleteQuery = 'DELETE FROM abc_orders WHERE order_id = ?';
-  var getOrderDetailsQuery = 'SELECT op.oid, op.pid, op.quantity, op.total_price FROM abc_orders_products AS op \
-                            INNER JOIN abc_products AS p ON op.pid = p.product_id \
-                            WHERE op.oid = ? ORDER BY op.pid';
+
+  var storeQuery = 'SELECT store_id, store_name FROM abc_stores';
+
+  // READ/ADD queries for Orders and Orders_Products
   var insertOrder = 'INSERT INTO abc_orders (`cid`, `sid`, `order_date`) VALUES (?, ?, ?)';
   var getLastOrderId = 'SELECT order_id FROM abc_orders ORDER BY order_id DESC LIMIT 1';
   var insertOrderProduct = 'INSERT INTO abc_orders_products (`pid`, `oid`, `quantity`, `total_price`) \
                             VALUES (?, ?, ?, ?)';
   var getUnitPrice = 'SELECT product_price FROM abc_products WHERE product_id = ?';
 
+  var getOrderDetailsQuery = 'SELECT op.oid, op.pid, p.product_name, op.quantity, op.total_price FROM abc_orders_products AS op \
+                            INNER JOIN abc_products as p ON op.pid = p.product_id \
+                            WHERE op.oid = ? ORDER BY op.pid';
+
+
+  // READ ORDERS helper function
   function getOrderInputList(results){
     var context = {'title': 'Orders', 'jsscripts': ['scripts', 'orders']};
     var inputList = [];
@@ -22,9 +31,20 @@ module.exports = function(){
       var data = results[i];
       var temp = {};
       temp['order_id'] = data.order_id;
-      temp['cid'] = data.cid;
-      temp['sid'] = data.sid;
 
+      // Check Null values for cid and sid
+      if (data.cid === null) {
+        temp['cid'] = 'Not Available';
+      } else {
+        temp['cid'] = data.cid;
+      };
+      if (data.sid === null) {
+        temp['sid'] = 'Not Available';
+      } else {
+        temp['sid'] = data.sid;
+      };
+
+      // Format date
       raw = data.order_date;
       year = raw.getFullYear();
       month = (raw.getMonth() + 1).toString();
@@ -37,23 +57,30 @@ module.exports = function(){
       };
       temp['order_date'] = year + "-" + month + "-" + date;
 
-      inputList.push(temp)
+      inputList.push(temp);
     };
 
     context['inputList'] = inputList;
-    JSON.stringify(context)
+    JSON.stringify(context);
 
     return context;
   }
 
+  // READ ORDERS
   function getOrders(req, res){
     var mysql = req.app.get('mysql');
 
     mysql.pool.query(getQuery, (error, results, fields) => {
-      res.render('orders', getOrderInputList(results))
-    })
+      if (error){
+        console.log(error);
+        res.send(error);
+        return;
+      };
+        res.render('orders', getOrderInputList(results));
+      });
   }
 
+  // FILTER ORDERS
   function searchOrder(req, res){
     var mysql = req.app.get('mysql');
     var orderId = req.body.orderId;
@@ -65,15 +92,15 @@ module.exports = function(){
     if (orderId.trim() !== "") {
       userInputs.push(orderId);
       queryPartial.push('order_id = ?');
-    }
+    };
     if (storeId.trim() !== "") {
       userInputs.push(storeId);
       queryPartial.push('sid = ?');
-    }
+    };
     if (custId.trim() !== "") {
       userInputs.push(custId);
       queryPartial.push('cid = ?');
-    }
+    };
 
     var condition = userInputs.length;
     if (condition > 1){
@@ -82,44 +109,160 @@ module.exports = function(){
     } else if (condition == 1){
       searchQuery += queryPartial[0];
     } else {
-        res.send("Error: No User Inputs.")
+        //res.send("Error: No User Inputs.");
+        res.redirect('/orders');
         return;
-    }
+    };
 
     mysql.pool.query(searchQuery, userInputs, (error, results, fields) => {
-      if(error) res.send(error);
-      else {
-        res.render('orders', getOrderInputList(results));
-        searchQuery = 'SELECT * FROM abc_orders WHERE ';
-    }})
+      if(error) {
+        console.log(error);
+        res.send(error);
+        return;
+      };
+      res.render('orders', getOrderInputList(results));
+      searchQuery = 'SELECT * FROM abc_orders WHERE ';
+    })
   }
 
-  async function deleteOrder(req, res){
+  // UPDATE ORDERS
+  async function updateOrder(req, res){
+    var mysql = req.app.get('mysql');
+    var orderId = req.body.order_id;
+    var cid = req.body.cid;
+    var sid = req.body.sid;
+    var orderDate = req.body.order_date;
+
+    await mysql.pool.query(updateQuery,[cid, sid, orderDate, orderId], (error)=>{
+      if (error) {
+        console.log(error);
+        res.send(error);
+        return;
+      }
+      res.redirect('/orders');
+    });
+  }
+
+  // DELETE ORDERS
+  function deleteOrder(req, res){
     var mysql = req.app.get('mysql');
     var orderId = req.body.id;
 
-    await mysql.pool.query(deleteQuery, orderId, (error) => {
-      if (error) res.send(error);
-    });
-
-    await mysql.pool.query(getQuery, (error, results, fields) => {
-        res.render('orders', getOrderInputList(results))
+    mysql.pool.query(deleteQuery, orderId, (error) => {
+      if (error) {
+        console.log(error);
+        res.send(error);
+        return;
+      }
+      res.redirect('/orders');
     });
   }
 
+  // ADD ORDERS - new window view
+  function addOrderView(req, res){
+    var mysql = req.app.get('mysql');
+    var context = {'title': 'Add Order', 'jsscripts': ['scripts', 'add_order']};
+    context['layout'] = 'noNav.handlebars';
+
+    mysql.pool.query(storeQuery, (error, results, fields) => {
+      var storeList = [];
+      for (i = 0; i < results.length; i++){
+        var temp = {};
+        var store = results[i];
+        temp['store_id'] = store.store_id;
+        temp['store_name'] = store.store_name;
+        storeList.push(temp);
+      }
+      context['storeList'] = storeList;
+      res.render('add_order', context);
+    });
+  }
+
+  // ADD ORDERS and ORDERS_PRODUCTS
+  async function addOrderInsert(req, res){
+    var mysql = req.app.get('mysql');
+
+    var cid = req.body.cid;
+    var sid = req.body.sid;
+    var order_date = req.body.orderDate;
+    var pids = req.body.pid;
+    var qtys = req.body.qty;
+
+    // add Orders first
+    mysql.pool.query(insertOrder, [cid, sid, order_date], (error) => {
+      if (error) {
+        console.log(error);
+        res.send(error);
+        return;
+      };
+    })
+
+    // calculate the total_price.
+    function addOrderInsertHelper (pid, quantity) {
+      mysql.pool.query(getUnitPrice, pid, (error, results, fields) => {
+        if (error) {
+          console.log('getUnitPrice Error: ' + adderror);
+          res.send(error);
+          return;
+        };
+        var total_price = quantity * results[0].product_price;
+
+        // Get the last added order_id for Orders
+        mysql.pool.query(getLastOrderId, (error, results, fields) => {
+          if (error) {
+            console.log('getLastOrderId Error: ' + error);
+            res.send(error);
+            return;
+          };
+          var oid = results[0].order_id;
+
+          // add Orders_Products
+          mysql.pool.query(insertOrderProduct, [pid, oid, quantity, total_price], (error) => {
+            if (error) {
+              console.log('insertOrderProduct Error: ' + error);
+              res.send(error);
+              return;
+            };
+          });
+        });
+      });
+    }
+
+    for (i = 0; i < pids.length; i ++) {
+      var pid = pids[i];
+      var quantity = qtys[i];
+      await addOrderInsertHelper(pid, quantity);
+    }
+    res.redirect('/add_order');
+  }
+
+  // READ ORDERS_PRODUCTS
   function getOrderDetails(req, res){
     var mysql = req.app.get('mysql');
     var orderId = req.params.id;
 
     var context = {'title': 'Order Details: ' + orderId, 'jsscripts': ['scripts', 'orders_products']};
     function renderOrderDetails(err, rows, fields){
-      JSON.stringify(rows)
+      if (err) {
+        console.log(err);
+        res.send(err);
+        return;
+      }
+
+      JSON.stringify(rows);
       var inputList = [];
       for(i = 0; i < rows.length; i++){
         data = rows[i];
         temp = {};
-        temp['id'] = data['order_id'];
-        temp['prodId'] = data['pid'];
+        temp['list_order'] = i;
+        temp['id'] = data['oid'];
+        if (data['pid'] === null) {
+          temp['prodId'] = 'Not Available';
+          temp['prodName'] = 'Not Available';
+        } else {
+          temp['prodId'] = data['pid'];
+          temp['prodName'] = data['product_name']
+        };
         temp['qty'] = data['quantity'];
         temp['total_price'] = data['total_price'];
         inputList.push(temp);
@@ -134,56 +277,12 @@ module.exports = function(){
       context['layout'] = 'noNav.handlebars';
       res.render('order_details', context);
     }
-    mysql.pool.query(getOrderDetailsQuery, orderId, renderOrderDetails)
-  }
-
-  function addOrderView(req, res){
-    var context = {'title': 'Add Order', 'jsscripts': ['scripts', 'add_order']};
-    context['layout'] = 'noNav.handlebars';
-
-    res.render('add_order', context);
-  }
-
-  async function addOrderInsert(req, res){
-    var mysql = req.app.get('mysql');
-
-    var cid = req.body.cid;
-    var sid = req.body.sid;
-    var order_date = req.body.orderDate;
-    var pids = req.body.pid;
-    var qtys = req.body.qty;
-
-    mysql.pool.query(insertOrder, [cid, sid, order_date], (error) => {
-      if (error) console.log(error);
-    })
-
-    function addOrderInsertHelper (pid, quantity) {
-      mysql.pool.query(getUnitPrice, pid, (error, results, fields) => {
-        if (error) console.log(error);
-        var total_price = quantity * results[0].product_price;
-
-        mysql.pool.query(getLastOrderId, (error, results, fields) => {
-          if (error) console.log(error);
-          var oid = results[0].order_id;
-
-          mysql.pool.query(insertOrderProduct, [pid, oid, quantity, total_price], (error) => {
-            if (error) console.log(error);
-          })
-        })
-      })
-    }
-
-    for (i = 0; i < pids.length; i ++) {
-      var pid = pids[i];
-      var quantity = qtys[i];
-      await addOrderInsertHelper(pid, quantity);
-    }
-
-    res.redirect('/add_order');
+    mysql.pool.query(getOrderDetailsQuery, orderId, renderOrderDetails);
   }
 
   router.get('/', getOrders)
   router.post('/', searchOrder)
+  router.put('/', updateOrder)
   router.delete('/', deleteOrder)
   router.get('/add_order', addOrderView)
   router.post('/add_order', addOrderInsert)
--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -4,33 +4,75 @@ function openAddOrder(event){
   window.open('orders/add_order', "_blank", "width=380px, height=600px");
 }
 
+// table event for order details, order update and delete functions.
+const orderTable = document.getElementById("orderTable");
+orderTable.onclick = function(event) {
+  var target = event.target;
+
+  if (target.name == 'details') {
+    requestDetail(event, target.value);
+  };
+  if (target.name == 'delete') {
+    requestDelete(event, target.value);
+    window.location.reload();
+  };
+  if (target.name == 'update') {
+    var inputElements = document.getElementsByClassName("input" + target.value);
+    var cidInput = inputElements[0];
+    var sidInput = inputElements[1];
+    var orderDateInput = inputElements[2];
+    var updateButton = inputElements[3];
+    cidInput.disabled = false;
+    sidInput.disabled = false;
+    orderDateInput.disabled = false;
+    event.preventDefault();
+
+    updateButton.textContent = 'Submit';
+    updateButton.setAttribute('class', "btn btn-info input" + target.value);
+    updateButton.addEventListener('click', (event) => {
+      var inputs = {};
+      inputs['order_id'] = target.value;
+      inputs['cid'] = target.form.cid.value;
+      inputs['sid'] = target.form.sid.value;
+
+      // cid user input validation
+      if (inputs['cid'].trim() === "" || isNaN(inputs['cid'])){
+        alert("Please Enter a valid customer Id");
+      } else if (inputs['cid'] == 'Not Available') {
+          inputs['cid'] = null;
+      };
+
+      // sid user input validation
+      if (inputs['sid'].trim() === "" || isNaN(inputs['sid'])){
+        alert("Please Enter a valid customer Id");
+      } else if (inputs['sid'] == 'Not Available') {
+          inputs['sid'] = null;
+      };
+
+      inputs['order_date'] = target.form.orderDate.value;
+      requestUpdate(event, inputs);
+      window.location.reload();
+    });
+  };
+};
+
+// read Orders_Products
 function requestDetail(event, id){
   window.open('orders/order_details/' + id, "_blank", "width=1000px, height=500px");
 }
 
+// request Order Delete
 function requestDelete(event, id){
   var req = new XMLHttpRequest();
-  var targetId = {id: id};
   req.open('DELETE', '/orders')
   req.setRequestHeader('Content-Type', 'application/json')
-  req.send(JSON.stringify(targetId));
+  req.send(JSON.stringify({id: id}));
 }
 
-function requestUpdate(event, id){
-  // TODO
+// request Order Update
+function requestUpdate(event, inputs){
+  var req = new XMLHttpRequest();
+  req.open('PUT', '/orders')
+  req.setRequestHeader('Content-Type', 'application/json')
+  req.send(JSON.stringify(inputs));
 }
-
-const productTable = document.getElementById("orderTable");
-productTable.onclick = function(event) {
-  var target = event.target;
-  if (target.name == 'details') {
-    requestDetail(event, target.value);
-  }
-  if (target.name == 'delete') {
-    requestDelete(event, target.value);
-  };
-  if (target.name == 'update') {
-    // TODO
-    // requestUpdate(event, target.value)
-  }
-};

4645b5b81f7732f09bc2ff75c96cdb74f2f70cb5
terrenceosu <59637780+terrenceosu@users.noreply.github.com>
2021-03-03 00:57:38 -0800

    Rough draft of Reports posted

--- db_app/customers_products.js
+++ db_app/customers_products.js
@@ -3,11 +3,31 @@ module.exports = function(){
   var express = require('express');
   var router = express.Router();
 
-  router.get('/', function(req, res){
-    var context = {'title': 'Customers and Products', 'script': 'customers_products'};
-    JSON.stringify(context);
-    res.render('customers_products', context)
-  });
+// Needs Age Calculation added to getCustomer function (Current Date - Birthdate)
 
-  return router;
+function getCustomer(res, mysql, context, complete) {
+  mysql.pool.query("SELECT customer_id, first_name, last_name FROM abc_customers", function (error, results, fields) {
+    if (error) {
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.customers = results;
+    complete();
+  });
+}
+
+router.get('/', function (req, res) {
+  var callbackCount = 0;
+  var context = {};
+  var mysql = req.app.get('mysql');
+  getCustomer(res, mysql, context, complete);
+  function complete() {
+    callbackCount++;
+    if (callbackCount >= 1) {
+      res.render('customers_products', context);
+    }
+  }
+});
+
+return router;
 }();
--- db_app/stores_products.js
+++ db_app/stores_products.js
@@ -3,10 +3,44 @@ module.exports = function(router){
   var express = require('express');
   var router = express.Router();
 
-  router.get('/', function(req, res){
-    var context = {'title': 'Stores and Prodcuts', 'script': 'stores_products'};
-    JSON.stringify(context);
-    res.render('stores_products', context)
+// NEED to Inner Join Stores with Orders and then take the sum of all quantities of each products for each store.
+
+  function getProducts(res, mysql, context, complete) {
+    mysql.pool.query("SELECT product_id, product_name FROM abc_products", function (error, results, fields) {
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.products = results;
+      complete();
+    });
+  }
+
+  function getStores(res, mysql, context, complete) {
+    mysql.pool.query("SELECT store_id, store_name FROM abc_stores", function (error, results, fields) {
+      if (error) {
+        res.write(JSON.stringify(error));
+        res.end();
+      }
+      context.stores = results;
+      complete();
+    });
+  }
+
+    // Route Store Data from DB
+
+  router.get('/', function (req, res) {
+    var callbackCount = 0;
+    var context = {};
+    var mysql = req.app.get('mysql');
+    getStores(res, mysql, context, complete);
+    getProducts(res, mysql, context, complete);
+    function complete() {
+      callbackCount++;
+      if (callbackCount >= 2) {
+        res.render('stores_products', context);
+      }
+    }
   });
 
   return router;

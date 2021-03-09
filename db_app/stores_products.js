4c89763a2342156b5c263163f40424cc09cab859
Leah In <23352423+leahin@users.noreply.github.com>
2021-03-09 12:31:45 -0500

    Completed Stores_Products

--- db_app/stores_products.js
+++ db_app/stores_products.js
@@ -1,26 +1,22 @@
 module.exports = function(router){
-
   var express = require('express');
   var router = express.Router();
 
+  var initProductId = 1;  // product_id initially shown on the page.
 
-var storeProd = 1;
+  var reportQuery = "SELECT s.store_id, s.store_name, p.product_id, p.product_name, IFNULL(SUM(op.quantity), 0) as total \
+            FROM abc_stores AS s \
+            INNER JOIN abc_orders AS o ON s.store_id = o.sid \
+            INNER JOIN abc_orders_products AS op ON o.order_id = op.oid \
+            INNER JOIN abc_products AS p ON op.pid = p.product_id \
+            WHERE product_id = ? \
+            GROUP BY s.store_id ORDER BY total DESC";
 
-  var reportQuery = "SELECT abc_stores.store_id, abc_stores.store_name, abc_products.product_name, IFNULL(SUM(abc_orders_products.quantity), 0) as Total \
-            FROM abc_orders_products \
-            JOIN abc_orders ON abc_orders_products.oid = abc_orders.order_id \
-            JOIN abc_stores ON abc_stores.store_id = abc_orders.sid \
-            JOIN abc_products ON abc_products.product_id = abc_orders_products.pid \
-            WHERE abc_products.product_id =" + storeProd +" \
-            Group BY abc_stores.store_id ASC";
+  var productQuery = "SELECT product_id, product_name FROM abc_products";
 
-// Need to change WHERE abc_products.product_id = 1 \ to the value selected in the drop down menu
-
-
-
-//Build dropdown filter get data to show the Products that are the database
+  //Build dropdown filter get data to show the Products that are the database
   function getProducts(res, mysql, context, complete) {
-    mysql.pool.query("SELECT product_id, product_name FROM abc_products", function (error, results, fields) {
+    mysql.pool.query(productQuery, function (error, results, fields) {
       if (error) {
         res.write(JSON.stringify(error));
         res.end();
@@ -31,30 +27,48 @@ var storeProd = 1;
   }
 
   // Get Data for table to show Store ID, Store Name, Product, Quantity Sold
-  function getStores(res, mysql, context, complete) {
-    mysql.pool.query(reportQuery, function (error, results, fields) {
+  function getStores(res, mysql, context, complete, product_id) {
+    mysql.pool.query(reportQuery, product_id, function (error, results, fields) {
       if (error) {
         res.write(JSON.stringify(error));
         res.end();
       }
       context.stores = results;
+      context.product_name = results[0].product_name;
       complete();
     });
   }
 
+  // the main page of stores and products. show product_id = 1
   router.get('/', function (req, res) {
     var callbackCount = 0;
     var context = {};
     var mysql = req.app.get('mysql');
-    getStores(res, mysql, context, complete);
+    getStores(res, mysql, context, complete, initProductId);
+    getProducts(res, mysql, context, complete);
+    function complete() {
+      callbackCount++;
+      if (callbackCount >= 2) {
+        res.render('stores_products', context);
+      };
+    };
+  });
+
+  // stores and products filter.
+  router.post('/', function (req, res) {
+    var callbackCount = 0;
+    var context = {};
+    let product_id = req.body.storeProd;
+    var mysql = req.app.get('mysql');
+    getStores(res, mysql, context, complete, product_id);
     getProducts(res, mysql, context, complete);
     function complete() {
       callbackCount++;
       if (callbackCount >= 2) {
         res.render('stores_products', context);
       }
-    }
-  });
+    };
+  })
 
   return router;
 }();
--- public/scripts/stores_products.js
+++
@@ -1,4 +0,0 @@
-function storeProducts(){
-    var userSelect = document.getElementById('storeProd').value
-    window.location = '/stores_products/filter' + parseInt(userSelect)
-}
